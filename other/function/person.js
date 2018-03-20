ad1d422b7a1690200bb2ff18e92bd45bfb7e9c2a
Ju DanDan <ddju@best-inc.com>
2018-03-20 17:43:36 +0800

    update

--- other/function/person.js
+++ other/function/person.js
@@ -10,7 +10,7 @@ function Person(first, last, age, gender, insterests) {
     this.insterests = insterests;
 }
 
-Person.prototype.greeting = function(){
+Person.prototype.greeting = function () {
     console.log('hi', this.name.first);
 }
 
@@ -21,3 +21,49 @@ function Teacher(first, last, age, gender, insterests, subject) {
 
     this.subject = subject
 }
+
+Teacher.prototype = Object.create(Person.prototype);
+Teacher.prototype.constructor = Teacher;
+
+var Aimi = new Teacher('Aimi', 'Alice', 20, 'F', 'reading', 'Chinese')
+
+console.log(Aimi);
+
+var count = 0;
+var i = 0
+for (i=0 ; i < 10; i++) {
+    console.log(i)
+}
+console.log(i)
+
+function markPerson(first, last) {
+
+    return {
+        first: first,
+        last: last,
+        fullName: function() {
+            return `${this.first} ${this.last}`
+        }
+    }
+    
+}
+
+var ami = markPerson('Ami', 'Col');
+var full = ami.fullName;
+console.log('fullName', full());
+console.log('fullName', ami.fullName());
+
+var co = new markPerson('Bro', 'Co');
+console.log('co', co, co.fullName());
+
+function RFullName(first, last) {
+    this.first = first;
+    this.last = last;
+}
+
+RFullName.prototype.fullName = function() {
+    return `${this.first} ${this.last}`;
+}
+
+var Ru = new RFullName('Ru', 'Co');
+console.log('RU', Ru, Ru.fullName());
